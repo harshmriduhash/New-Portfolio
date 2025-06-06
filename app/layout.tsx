c4512702e75473484fa6b671fe553f15ff1a41fa
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-06-06 23:25:53 +0530

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   title: "Suyash's Portfolio",
   description:
     "Hey! I'm Suyash, your go-to web developer, turning ideas into pixels and code.",
-    icons: "/profile.jpeg"
+  icons: "/profile.jpeg",
 };
 
 export default function RootLayout({
