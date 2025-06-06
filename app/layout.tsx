af6ad5051593913235d89c4cf7afd98dbea8e81d
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-06-06 23:26:43 +0530

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Suyash's Portfolio",
+  title: "Harsh's Portfolio",
   description:
     "Hey! I'm Harsh, your go-to web developer, turning ideas into pixels and code.",
   icons: "/profile.jpeg",
